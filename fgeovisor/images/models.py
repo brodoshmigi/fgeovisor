aa39464ffa88713a764f254f5854b73db242b050
shi1i <Hyperb0lA@yandex.ru>
2024-11-19 14:44:21 +0300

    micro fix

--- fgeovisor/images/models.py
+++ fgeovisor/images/models.py
@@ -6,6 +6,6 @@ class Image(models.Model):
     polygon = models.ForeignKey(Polygon, related_name=('images'), 
                                     on_delete=models.CASCADE)
     # Ссылка на изображение или путь в хранилище
-    url = models.ImageField(upload_to='fgeovisor/site_back/IMAGES')
+    url = models.ImageField(upload_to='fgeovisor/web_interface/IMAGES')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
