a64846a64aa08a1cab7259f09b730dce1cef62e6
agrovain <165189957+agrovain@users.noreply.github.com>
2024-11-22 09:03:07 +0300

    merge with main
    Signed-off-by: agrovain <165189957+agrovain@users.noreply.github.com>

--- fgeovisor/images/staff.py
+++ fgeovisor/images/staff.py
@@ -17,7 +17,7 @@ from polygons.serializators import PolygonFromDbSerializer
 
 
 ee.Authenticate()
-ee.Initialize(project='ee-nezhentsev2017')
+ee.Initialize(project='ee-cocafin1595')
 IMAGE_DIR = path.dirname(path.abspath(__file__)) + '/IMAGES'
 
 
