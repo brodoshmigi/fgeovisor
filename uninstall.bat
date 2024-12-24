3bb339825c0d1d73aa91553d5c49a9088470ecc8
unknown <nezhentsev2017@gmail.com>
2024-12-24 10:43:40 +0300

    modify uninstall.bat

--- uninstall.bat
+++ uninstall.bat
@@ -4,3 +4,10 @@ set "current_dir=%current_dir:~0,-1%"
 for %%a in ("%current_dir%") do set folder_name=%%~nxa
 reg delete "HKCU\Software\MyScript" /v FirstRun /f
 rmdir /s /q  "%folder_name%_venv"
+echo Вы уверены, что хотите удалить все файлы? (Y/N)
+choice /c YN /n
+if errorlevel 2 goto :end
+
+rmdir /s /q "%current_dir%"
+
+:end
