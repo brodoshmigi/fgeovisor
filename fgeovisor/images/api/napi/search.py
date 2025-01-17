6b02c562d4289c52a45dcd1ff98fabe973a53c34
shi1i <Hyperb0lA@yandex.ru>
2025-01-17 15:37:20 +0300

    add some interesting features for future

--- fgeovisor/images/api/napi/search.py
+++ fgeovisor/images/api/napi/search.py
@@ -53,6 +53,12 @@ class SearchCatalog():
 
 
 class ClientPool():
+    """
+    Здесь нужно будет добавить входящими аргументы.
+
+    Но это необходимо только, если собираемся использовать более низкий уровень
+    библиотеки pystac_client.
+    """
 
     def __init__(self):
         self.clients = {}
@@ -160,9 +166,9 @@ class SearchCollections():
         self.catalog = SearchCatalog().get_sort_childs(orderby=catalog_list)
         self.clients_pool = clients_pool
 
-    def get_org_catalogs(self,
-                         area: Tuple | List = None,
-                         date: str = None) -> pd.DataFrame:
+    def get_by_orgs(self,
+                    area: Tuple | List = None,
+                    date: str = None) -> pd.DataFrame:
         """
         **Ищет каталоги, которые соответствуют нашему запросу.**
         
@@ -179,18 +185,39 @@ class SearchCollections():
             data = pd.DataFrame([{
                 'id': c['id'],
                 'href': link
-            } for c in self._get_collection(
+            } for c in self._search_collection(
                 link=link, bbox=area, datetime=date)])
             filtered = data[data['id'].str.lower().apply(
                 lambda x: any(search in x for search in self.satelite_names))]
             true_collections = pd.concat([true_collections, filtered])
         return true_collections
+    
+    def get_by_link(self):
+        for link in self.catalog:
+            return self._get_collections(link=link)
 
-    def _get_collection(self, link: str, **kwargs):
+    def _search_collection(self, link: str, **kwargs):
         client: Client = self.clients_pool.get_client(link=link)
         collections = client.collection_search(q='landsat', **kwargs)
         return collections.collections_as_dicts()
 
+    def _get_collections(self, link: str):
+        # Маленький шаг для человека, но огромный для меня
+        # Большее понимание библиотеки находится здесь...
+        client: Client = self.clients_pool.get_client(link=link)
+        result = pd.DataFrame([{
+            'r': c.id,
+            'link': link,
+        } for c in client.get_collections()])
+        return result.drop_duplicates()
+    
+    def _get_collection(self, link: str, id: str):
+        client: Client = self.clients_pool.get_client(link=link)
+        result = pd.DataFrame([{
+            'r': c.id,
+            'link': link,
+        } for c in client.get_collection(id)])
+        return result
 
 class Assets(ABC):
 
