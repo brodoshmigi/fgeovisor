0bac394d18ab4be0ad96a6f040c29ac9a63727aa
shi1i <Hyperb0lA@yandex.ru>
2025-02-24 22:44:12 +0300

    edit admin imports

--- fgeovisor/web_interface/admin.py
+++ fgeovisor/web_interface/admin.py
@@ -2,16 +2,17 @@ from django.contrib import admin
 from django.contrib.gis import admin
 from django.contrib.gis.forms.widgets import OSMWidget
 from .models import SessionStorage, ActivityLog
-from polygons.models import Polygon
-from images.models import Image
+from polygons.models import UserPolygon, ImageBounds
+from images.models import UserImage, NasaImage
 
-# Добавление моделей через декоратор, + вывод карты, если модель имеет такое поле
-@admin.register(Polygon, Image)
+
+@admin.register(UserPolygon, ImageBounds, UserImage, NasaImage)
 class admin_overview(admin.GISModelAdmin):
     gis_widget = OSMWidget
     gis_widget.default_lat = 45.04
     gis_widget.default_lon = 41.97
     gis_widget.default_zoom = 15
+
 
 @admin.register(SessionStorage, ActivityLog)
 class admin_overview_log_and_session(admin.ModelAdmin):
