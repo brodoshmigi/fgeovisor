f3abf3530a2d53de283dfd27ee90374f2fef9b21
shi1i <Hyperb0lA@yandex.ru>
2025-02-24 22:28:47 +0300

    renaming

--- fgeovisor/images/models.py
+++ fgeovisor/images/models.py
@@ -1,19 +1,19 @@
 from abc import abstractmethod
 
 from django.db import models
-from polygons.models import Polygon
-
-
+from polygons.models import UserPolygon, ImageBounds
 """ check() к сожалению нарушает DRY ((( из-за этого никто не апнет предатора в апексе """
 # 24.02.2025
+
 
 class Image(models.Model):
     """ Абстрактная модель изображения """
 
     _next_handler: models.Model = None
 
-    local_uri = models.ImageField(upload_to='fgeovisor/images/IMAGES', null=True)
-    real_uri = models.URLField(unique=True, null=True)
+    local_uri = models.ImageField(upload_to='fgeovisor/images/IMAGES',
+                                  null=True)
+    cloud_uri = models.URLField(unique=True, null=True)
     image_date = models.DateField()
 
     def set_next(self, h: models.Model) -> models.Model:
@@ -30,11 +30,12 @@ class Image(models.Model):
     class Meta:
         abstract = True
 
+
 class ImageType(models.Model):
     """ Абстрактный миксин для добавления коэффициентов """
 
     # len = 6
-    enum_ratio = {
+    enum_index = {
         'NDVI': 'ndvi',     # Вегетационный индекс растительности
         'EVI': 'evi',       # Улучшенный индекс растительности
         'SAVI': 'savi',     # Индекс растительности с поправкой на почву
@@ -42,16 +43,18 @@ class ImageType(models.Model):
         'MSAVI': 'msavi',   # Модификация savi
         'NDRE': 'ndre'      # Нормализованный индекс красного края (хз что это)
     }
-    image_type = models.CharField(max_length=5,
-                                  choices=enum_ratio,
-                                  default='NDVI')
-    
+    image_index = models.CharField(max_length=5,
+                                   choices=enum_index,
+                                   default='NDVI')
+
     class Meta:
         abstract = True
 
+
 class NasaImage(Image):
     """ Example for NASA cloud-schema image (usually json) """
-    poly_bounds = models.ForeignKey(Polygon,
+    poly_bounds = models.ForeignKey(to=ImageBounds,
+                                    to_field='polygon_id',
                                     related_name=('nasa_image'),
                                     on_delete=models.CASCADE)
     local_uri = models.JSONField(null=True)
@@ -71,7 +74,8 @@ class NasaImage(Image):
 
 class UserImage(Image, ImageType):
     """ Модель изображения для полигона пользователя """
-    polygon_id = models.ForeignKey(Polygon,
+    polygon_id = models.ForeignKey(to=UserPolygon,
+                                   to_field='polygon_id',
                                    related_name=('user_image'),
                                    on_delete=models.CASCADE)
 
