a8f06e07e8100a3da41b319025368e10b2c4d162
shi1i <Hyperb0lA@yandex.ru>
2025-02-24 22:07:17 +0300

    models refactoring

--- fgeovisor/images/models.py
+++ fgeovisor/images/models.py
@@ -1,12 +1,88 @@
+from abc import abstractmethod
+
 from django.db import models
 from polygons.models import Polygon
 
-# Модель изображения
+
+""" check() к сожалению нарушает DRY ((( из-за этого никто не апнет предатора в апексе """
+# 24.02.2025
+
 class Image(models.Model):
-    polygon = models.ForeignKey(Polygon, related_name=('images'), 
+    """ Абстрактная модель изображения """
+
+    _next_handler: models.Model = None
+
+    local_uri = models.ImageField(upload_to='fgeovisor/images/IMAGES', null=True)
+    real_uri = models.URLField(unique=True, null=True)
+    image_date = models.DateField()
+
+    def set_next(self, h: models.Model) -> models.Model:
+        self._next_handler = h
+        return h
+
+    @abstractmethod
+    def check(self, request):
+        if self._next_handler:
+            return self._next_handler.check()
+
+        return None
+
+    class Meta:
+        abstract = True
+
+class ImageType(models.Model):
+    """ Абстрактный миксин для добавления коэффициентов """
+
+    # len = 6
+    enum_ratio = {
+        'NDVI': 'ndvi',     # Вегетационный индекс растительности
+        'EVI': 'evi',       # Улучшенный индекс растительности
+        'SAVI': 'savi',     # Индекс растительности с поправкой на почву
+        'GNDVI': 'gndvi',   # NDVI только с зеленым каналом
+        'MSAVI': 'msavi',   # Модификация savi
+        'NDRE': 'ndre'      # Нормализованный индекс красного края (хз что это)
+    }
+    image_type = models.CharField(max_length=5,
+                                  choices=enum_ratio,
+                                  default='NDVI')
+    
+    class Meta:
+        abstract = True
+
+class NasaImage(Image):
+    """ Example for NASA cloud-schema image (usually json) """
+    poly_bounds = models.ForeignKey(Polygon,
+                                    related_name=('nasa_image'),
                                     on_delete=models.CASCADE)
-    # Ссылка на изображение или путь в хранилище
-    url = models.ImageField(upload_to='fgeovisor/images/IMAGES')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    date = models.DateField()
+    local_uri = models.JSONField(null=True)
+
+    def check(self, request) -> str:
+        if self.real_uri:
+            return self.real_uri
+
+        if self.local_uri:
+            return self.local_uri
+
+        return super().check(request)
+
+    class Meta:
+        ordering = ['image_date']
+
+
+class UserImage(Image, ImageType):
+    """ Модель изображения для полигона пользователя """
+    polygon_id = models.ForeignKey(Polygon,
+                                   related_name=('user_image'),
+                                   on_delete=models.CASCADE)
+
+    def check(self, request) -> str:
+        if self.real_uri:
+            return self.real_uri
+
+        if self.local_uri:
+            return self.local_uri
+
+        return super().check(request)
+
+    class Meta:
+        ordering = ['polygon_id']
