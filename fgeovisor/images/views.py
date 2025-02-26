b1604d040fa633e3545101a6f0ad236a831b8173
shi1i <Hyperb0lA@yandex.ru>
2025-02-26 15:36:28 +0300

    validate request query on backend

--- fgeovisor/images/views.py
+++ fgeovisor/images/views.py
@@ -6,7 +6,7 @@ from rest_framework.mixins import (ListModelMixin, UpdateModelMixin,
 from rest_framework.status import (HTTP_200_OK, HTTP_204_NO_CONTENT,
                                    HTTP_201_CREATED,
                                    HTTP_500_INTERNAL_SERVER_ERROR,
-                                   HTTP_400_BAD_REQUEST)
+                                   HTTP_400_BAD_REQUEST, HTTP_302_FOUND)
 
 from rest_framework.response import Response
 from polygons.models import UserPolygon
@@ -15,8 +15,10 @@ from .models import UserImage
 from .serializators import ImageSerializator
 from .staff import Image_GEE
 
+DEFAULT_PARAMS = {'id': '', 'date': '', 'index': ''}
 
-class UploadImg(GenericViewSet, ListModelMixin, UpdateModelMixin, 
+
+class UploadImg(GenericViewSet, ListModelMixin, UpdateModelMixin,
                 CreateModelMixin, DestroyModelMixin):
     permission_classes = [IsAuthenticated]
 
@@ -24,29 +26,51 @@ class UploadImg(GenericViewSet, ListModelMixin, UpdateModelMixin,
 
     def get_queryset(self):
         query_params = self.request.GET
+
         polygon_id, index, date = query_params['id'], \
             query_params['index'].upper(), query_params['date']
+
         polygon_object = UserPolygon.objects.get(polygon_id=polygon_id)
         return UserImage.objects.filter(polygon_id=polygon_object,
                                         image_index=index,
                                         image_date=date)
-    
+
     def list(self, request, *args, **kwargs) -> Response:
-        queryset : UserImage = self.filter_queryset(self.get_queryset())
-        polygon_object = UserPolygon.objects.get(polygon_id=self.request.GET['id'])
-        
+
+        if not self.is_query_valid(request.GET):
+            error = {
+                'error':
+                f'You forgot {DEFAULT_PARAMS.keys()-request.GET.keys()}'
+            }
+            return Response(status=HTTP_400_BAD_REQUEST, data=error)
+
+        queryset: UserImage = self.filter_queryset(self.get_queryset())
+        polygon_object = UserPolygon.objects.get(
+            polygon_id=self.request.GET['id'])
+
         if not queryset:
             #если скачиваются ужен скачанные снимки, воможно проблема в датах
-            image_object = Image_GEE(polygon_object,date_start=self.request.GET['date'])
+            image_object = Image_GEE(polygon_object,
+                                     date_start=self.request.GET['date'])
             image_object.download_image()
             _image_object = image_object.calculate_index()
-            return Response(status=HTTP_201_CREATED, data=_image_object.check_uri(request='1'))
-        
+            return Response(status=HTTP_201_CREATED,
+                            data={'url': _image_object.check_uri(request='1')})
+
         serializer = self.get_serializer(queryset, many=True)
         image_uri = queryset[0].check_uri(request='1')
         if image_uri is not None:
-            return Response(image_uri)
-        return Response(serializer.data)
+            return Response({'url': image_uri}, status=HTTP_302_FOUND)
+        
+        return Response(serializer.data, status=HTTP_204_NO_CONTENT)
+
+    def is_query_valid(self, query_dict):
+        match query_dict.keys().__len__():
+            case 0 | 1 | 2:
+                return False
+            case _:
+                return True
+
 
 class ImageGEE(APIView):
     """
@@ -72,5 +96,3 @@ class ImageGEE(APIView):
         polygon_image.visualization()
         My_errors.tmp_context['photo'] = True
         return Response(My_errors.error_send())
-
-
