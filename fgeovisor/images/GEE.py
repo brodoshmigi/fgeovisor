526391eccb3c31cbee56d2010840cf44084203d5
agrovain <nezhentsev2017@gmail.com>
2025-03-18 22:01:10 +0300

    im durach0k

--- fgeovisor/images/GEE.py
+++ fgeovisor/images/GEE.py
@@ -29,7 +29,7 @@ def update_image_GEE(polygon):
     new_image.visualization()
 
 
-IMAGE_DIR = 'images/IMAGES' if name != 'posix' else 'images/IMAGES'
+IMAGE_DIR = 'images/IMAGES'
 
 RATIO_ENUM_S2_BANDS = {
         'NDVI': ['B4', 'B8'],            # Вегетационный индекс растительности
