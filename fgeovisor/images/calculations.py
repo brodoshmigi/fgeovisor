efc3ecc9ef0ea332f9796755919011bb78984ac3
CocaFin <brykalov1012@gmail.com>
2025-03-18 15:45:11 +0300

    index calculations fix

--- fgeovisor/images/calculations.py
+++ fgeovisor/images/calculations.py
@@ -1,59 +1,45 @@
 from numpy import (seterr, ndarray, set_printoptions, nan)
 from sys import maxsize
-from osgeo.gdal import (Dataset, Band, Open, Warp, UseExceptions, GDT_Float32, )
-from django.contrib.gis.gdal.raster.source import GDALRaster
-from django.contrib.gis.gdal.raster.band import GDALBand
-from matplotlib.pyplot import imsave, imshow
-from copy import copy
+from osgeo.gdal import (Dataset, Open, Warp, UseExceptions, GDT_Float64, )
+from json import dump
 
 set_printoptions(threshold=maxsize)
 UseExceptions()
 
 def calculate_NDVI(red: ndarray, nir: ndarray):
-    '''
-    normalization (-1, 1)
-    '''
-    seterr(divide='ignore', invalid='ignore')
+    '''normalization (-1, 1)'''
+    #seterr(divide='ignore', invalid='ignore')
     ndvi = (nir - red) / (nir + red)
+    ndvi = ndvi.round(2)
     return ndvi
 
 def calculate_EVI(blue: ndarray, red: ndarray, nir: ndarray,
                   L: float = 1, C1: float = 6, C2: float = 7.5, G: float = 2.5):
-    '''
-    normalization (0, 1)
-    '''
+    '''normalization (0, 1)'''
     evi = G * (nir - red) / (nir + C1 * red - C2 * blue + L)
     return evi
     
 def calculate_SAVI(red: ndarray, nir: ndarray, l: float = 0.5):
-    '''
-    normalization (-1, 1)
-    '''
-    seterr(divide='ignore', invalid='ignore')
+    '''normalization (-1, 1)'''
+    #seterr(divide='ignore', invalid='ignore')
     savi = (1 + l) * ((nir - red) / (nir + red + l))
     return savi
     
 def calculate_GNDVI(green: ndarray, nir: ndarray):
-    '''
-    normalization (-1, 1)
-    '''
-    seterr(divide='ignore', invalid='ignore')
+    '''normalization (-1, 1)'''
+    #seterr(divide='ignore', invalid='ignore')
     ndre = (nir - green) / (nir + green)
     return ndre
 
 def calculate_MSAVI(red: ndarray, nir: ndarray):
-    '''
-    normalization (-1, 1)
-    '''
-    seterr(divide='ignore', invalid='ignore')
+    '''normalization (-1, 1)'''
+    #seterr(divide='ignore', invalid='ignore')
     msavi = (1 / 2) * (2 * (nir + 1) - (((2 * nir + 1) ** 1/2 ) * 2 - 8 * (nir - red)))
     return msavi
 
 def calculate_NDRE(red_edge: ndarray, nir: ndarray):
-    '''
-    normalization (-1, 1)
-    '''
-    seterr(divide='ignore', invalid='ignore')
+    '''normalization (-1, 1)'''
+    #seterr(divide='ignore', invalid='ignore')
     ndre = (nir - red_edge) / (nir + red_edge)
     return ndre
 
@@ -65,7 +51,7 @@ def crop_image(url_raster: str, geo_json, index: ndarray):
     src_ds: Dataset = Open(url_raster)
     memory_ds: Dataset = Warp('', src_ds, 
                                 format='MEM', 
-                                outputType=GDT_Float32, 
+                                outputType=GDT_Float64, 
                                 dstNodata=nan)
     src_ds.Close()
 
